7c85d9ebf92ecf739e642c93c052d69cfc0d82a5
Luan Paredes <luan.developer.ti@gmail.com>
2022-04-14 00:47:20 -0300

    [HM-0000] Main file added

---
+++ main.py
@@ -0,0 +1,23 @@
+from controller.screen_management import ScreenManagement
+from kivymd.app import MDApp
+from kivy.core.window import Window
+
+
+#App size
+Window.minimum_width, Window.minimum_height = (300, 650)
+
+
+class MainApp(MDApp):
+    def build(self):
+        self.title = 'Hangman - The Master'
+        self.theme_cls.primary_palette = 'Indigo'
+        self.theme_cls.accent_palette = 'Gray'
+        self.theme_cls.primary_hue = '500'
+        self.theme_cls.accent_hue = '300'
+        self.theme_cls.theme_style = 'Dark'
+
+        return ScreenManagement()
+
+
+if __name__ == '__main__':
+    MainApp().run()
